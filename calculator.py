e2a4a2cfb25f7693c2e325cdc60721194d2f9ba3
kadupradumya53-commits <kadupradumya53@gmail.com>
2026-09-13 12:05:29 +0530

    Update calculator.py
    This is an simple calculator  I made while learning python.
    
    It  takes two numbers and an operator (+ ,-  , *,  /) from the user and peforms the selected operation.
    
    Imade this project to practice if-elif-else, variable, input, and basic operators.

--- calculator.py
+++ calculator.py
@@ -1,7 +1,15 @@
 num1 = int(input("Enter a first number:"))
+operator = input("enter a operator (+,-,*,/): ")
 num2 = int(input("Enter a second number:"))
 
-print("Addition:",num1 + num2)
-print("Subtraction:",num1 - num2)
-print("Multiplication:",num1 * num2)
-print("Division:",num1 / num2)
+if operator == '+':
+   print("Addition:",num1 + num2)
+   
+elif operator == '-':
+        print("Subtraction:",num1 - num2)
+
+elif operator == '*':
+    print("Multiplication:",num1 * num2)
+
+elif operator == '/':
+    print("Division:",num1 / num2))
